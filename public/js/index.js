93bad1d26e0bee67a006e78a2a9c1a5929be28a9
HeebieGeeBee <geoffbull29@hotmail.co.uk>
2017-04-11 23:35:30 +0200

    first

---
+++ public/js/index.js
@@ -0,0 +1,38 @@
+var socket;
+
+function setup() {
+	createCanvas(500, 500);
+	background(150,150,150);
+	socket = io.connect('http://localhost:3000');
+	socket.on('mouse', newDrawing);
+}
+
+function newDrawing(data) {
+	noStroke();
+	fill(random(0, 255));
+	ellipse(data.x, data.y, 20, 20);
+}
+
+function mouseDragged() {
+	console.log('Sending: ' + mouseX + ',' + mouseY);
+	var data = {
+		x: mouseX,
+		y: mouseY
+	}
+
+	socket.emit('mouse', data)
+
+	fill(255);
+	noStroke();
+	ellipse(mouseX, mouseY, 20, 20);
+	
+	
+}
+
+function draw() {
+	
+	
+	
+	//console.log(mouseX + ',' + mouseY);
+
+}
