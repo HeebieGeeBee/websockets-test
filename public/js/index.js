405b2e9217e5cb4ef8384e99fd0cd9a4215ec93b
HeebieGeeBee <geoffbull29@hotmail.co.uk>
2017-04-12 03:55:04 +0200

    fourth commit three

--- public/js/index.js
+++ public/js/index.js
@@ -2,28 +2,34 @@ var socket;
 
 function setup() {
 	createCanvas(window.innerWidth, window.innerHeight);
-	background(150,150,150);
+	background(0);
 	socket = io.connect('https://websocket-test-005.herokuapp.com/');
 	socket.on('mouse', newDrawing);
 }
 
 function newDrawing(data) {
-	noStroke(255);
-	fill(random(0, 255), 120, 120);
+	noStroke();
+	fill(data.color);
 	ellipse(data.x, data.y, 20, 20);
 }
 
 function mouseDragged() {
+	fill(random(0, 120), random(0, 120), random(0, 120));
+	console.log(color);
+	noStroke();
+	ellipse(mouseX, mouseY, 20, 20);
+
+
+	var color = this.drawingContext.fillStyle; 
 	var data = {
 		x: mouseX,
-		y: mouseY
+		y: mouseY,
+		color: color
 	}
 
 	socket.emit('mouse', data)
 
-	fill("red");
-	noStroke();
-	ellipse(mouseX, mouseY, 20, 20);
+	
 	
 	
 }
